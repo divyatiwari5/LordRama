7b7fc5f66adeb6dd30c7d109b035f67fc8a1fc0e
divyatiwari5 <tiwari.divya50@gmail.com>
2018-11-02 18:36:14 +0530

    Hnadled few errors

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ const LaunchRequestHandler = {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
     },
     handle(handlerInput) {
-        const speechText = 'Welcome to Lord Ramas kingdom!';
-        const repromptText = 'You can ask about Rama`s parents, wife, brothers, his birthplace, his Devotee - Hanuman, Ravana and his empire Lanka';
+        const speechText = 'Welcome to Lord Rama`s kingdom!';
+        const repromptText = 'You can ask about Rama`s wife, brothers, his birthplace, his Devotee - Hanuman, Ravana and his empire Lanka';
         console.log(speechText);
-        //   handlerInput.attributesManager.setSessionAttributes({'name': 'investment'});
         return handlerInput.responseBuilder
             .speak(speechText + repromptText)
             .reprompt(speechText + repromptText)
@@ -33,8 +32,10 @@ const AboutRamaHandler = {
        handlerInput.requestEnvelope.request.intent.name === 'AboutRama';
      },
     handle(handlerInput) {
-        const speechText = 'Ram, also known as Ramachandra, is a major deity of Hinduism. He was the seventh avatar of the god Vishnu.';
-        const repromptText = 'Ram, also known as Ramachandra, is a major deity of Hinduism. He was the seventh avatar of the god Vishnu.';
+        const speechText = 'Ram, also known as Ramachandra, is a major deity of Hinduism.  He was the seventh avatar of the god Vishnu. Want to know more about him?' +
+            'Ask what`s his birthplace, who is his wife, who was Hanuman?';
+        const repromptText = 'Ram, also known as Ramachandra, is a major deity of Hinduism. He was the seventh avatar of the god Vishnu. Want to know more about him?' +
+            'Ask what`s his birthplace, who is his wife, who was Hanuman?';
         console.log(speechText);
         return handlerInput.responseBuilder
             .speak(speechText)
@@ -54,36 +55,13 @@ const BirthplaceHandler = {
        handlerInput.requestEnvelope.request.intent.name === 'Birthplace';
      },
     handle(handlerInput) {
-        const speechText = 'Rama\'s birthplace is on the banks of the Sarayu river in a city called Ayodhya in Uttar Pradesh, India.';
-        const repromptText = 'Rama\'s birthplace is on the banks of the Sarayu river in a city called Ayodhya in Uttar Pradesh, India.';
-        console.log(speechText);
-        //   handlerInput.attributesManager.setSessionAttributes({'name': 'investment'});
+        const speechText = 'Rama`s birthplace is on the banks of the Sarayu river in a city called Ayodhya in Uttar Pradesh, India.';
+        const repromptText = 'Rama`s birthplace is on the banks of the Sarayu river in a city called Ayodhya in Uttar Pradesh, India.';
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(repromptText)
             .withSimpleCard(title, speechText)
-            .getResponse();
-    }
-};
-
-/***
- * Ram's parent details handler
- * @type {{canHandle(*): *, handle(*): *}}
- */
-const ParentalHistoryHandler = {
-    canHandle(handlerInput) {
-     return handlerInput.requestEnvelope.request.type === 'IntentRequest'&&
-       handlerInput.requestEnvelope.request.intent.name === 'ParentalHistory';
-     },
-    handle(handlerInput) {
-        const speechText = 'Rama was born to Kaushalya & Dashratha in Ayodhya. His father was the ruler kingdom of Kosala.';
-        const repromptText = 'Rama was born to Kaushalya & Dashratha in Ayodhya. His father was the ruler kingdom of Kosala.';
-        console.log(speechText);
-        //   handlerInput.attributesManager.setSessionAttributes({'name': 'investment'});
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText )
-            .withSimpleCard(title, speechText)
+            .withShouldEndSession(true)
             .getResponse();
     }
 };
@@ -98,15 +76,15 @@ const WifeDetailsHandler = {
        handlerInput.requestEnvelope.request.intent.name === 'WifeDetails';
      },
     handle(handlerInput) {
-        const speechText = 'He got married to Sita, an avatar of Sri Lakshmi. She is described as the daughter of the earth goddess, Bhūmi and the adopted daughter' +
+        const speechText = 'Ram got married to Sita, an avatar of Sri Lakshmi. She is described as the daughter of the earth goddess, Bhūmi and the adopted daughter' +
             ' of King Janaka of Videha and his wife, Queen Sunaina. ';
-        const repromptText = 'He got married to Sita, an avatar of Sri Lakshmi. She is described as the daughter of the earth goddess, Bhūmi and the adopted daughter\' +\n' +
+        const repromptText = 'Ram got married to Sita, an avatar of Sri Lakshmi. She is described as the daughter of the earth goddess, Bhūmi and the adopted daughter\' +\n' +
             '            \' of King Janaka of Videha and his wife, Queen Sunaina.';
-        console.log(speechText);
         return handlerInput.responseBuilder
             .speak(speechText )
             .reprompt(speechText )
             .withSimpleCard(title, speechText )
+            .withShouldEndSession(true)
             .getResponse();
     }
 };
@@ -123,12 +101,11 @@ const HanumanHandler = {
     handle(handlerInput) {
         const speechText = 'Hanuman was Rama`s ardent devotee. He was the son of Anjana and Kesari and is also son of the wind-god Vayu.';
         const repromptText = 'Hanuman was Rama`s ardent devotee. He was the son of Anjana and Kesari and is also son of the wind-god Vayu.';
-        console.log(speechText);
-        //   handlerInput.attributesManager.setSessionAttributes({'name': 'investment'});
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(speechText)
             .withSimpleCard(title, speechText)
+            .withShouldEndSession(true)
             .getResponse();
     }
 };
@@ -143,38 +120,13 @@ const LankaHandler = {
        handlerInput.requestEnvelope.request.intent.name === 'LankaDetails';
      },
     handle(handlerInput) {
-        const speechText = 'Lanka is considered to be a large island-country, situated in the Indian Ocean. Lanka is the name given in Hindu epics to the island fortress capital of the legendary asura king Ravana in the epics of the Ramayana and the Mahabharata.' +
-            'The ancient city of Lankapura is thought to have been burnt down by Hanuman';
-        const repromptText = 'Lanka is considered to be a large island-country, situated in the Indian Ocean. Lanka is the name given in Hindu epics to the island fortress capital of the legendary asura king Ravana in the epics of the Ramayana and the Mahabharata.' +
-            'The ancient city of Lankapura is thought to have been burnt down by Hanuman';
-        console.log(speechText);
-        //   handlerInput.attributesManager.setSessionAttributes({'name': 'investment'});
+        const speechText = 'Lanka is considered to be a large island-country, situated in the Indian Ocean. Lanka is the name given in Hindu epics to the island fortress capital of the legendary asura king Ravana in the epics of the Ramayana and the Mahabharata. ' +
+            'The ancient city of Lankapura is thought to have been burnt down by Hanuman.';
+        const repromptText = 'Lanka is considered to be a large island-country, situated in the Indian Ocean. Lanka is the name given in Hindu epics to the island fortress capital of the legendary asura king Ravana in the epics of the Ramayana and the Mahabharata. ' +
+            ' The ancient city of Lankapura is thought to have been burnt down by Hanuman. Ask who was the emperor of Lanka?  ';
         return handlerInput.responseBuilder
             .speak(speechText )
             .reprompt(speechText )
-            .withSimpleCard(title, speechText)
-            .getResponse();
-    }
-};
-
-/***
- *
- * Ram's Family Details Intent Handler
- * @type {{canHandle(*): *, handle(*): *}}
- */
-const FamilyHandler = {
-    canHandle(handlerInput) {
-     return handlerInput.requestEnvelope.request.type === 'IntentRequest'&&
-       handlerInput.requestEnvelope.request.intent.name === 'FamilyDetails';
-     },
-    handle(handlerInput) {
-        const speechText = 'He had 3 siblings: Lakshmana, Bharata, and Shatrughna. Lakshmana married to Sita`s sister Urmila. Bharata married to Mandavi & Shatrughna married to Shrutakirti.';
-        const repromptText = 'He had 3 siblings: Lakshmana, Bharata, and Shatrughna. Lakshmana married to Sita`s sister Urmila. Bharata married to Mandavi & Shatrughna married to Shrutakirti.';
-        console.log(speechText);
-        //   handlerInput.attributesManager.setSessionAttributes({'name': 'investment'});
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText)
             .withSimpleCard(title, speechText)
             .getResponse();
     }
@@ -191,16 +143,16 @@ const RavanaHandler = {
      },
     handle(handlerInput) {
         const speechText = 'Ravana was a follower of Shiva, a great scholar, a capable ruler and a maestro of the veena, but someone who wished to overpower the Devas.' +
-            ' His ten heads represent his knowledge of the six shastras and the four Vedas.';
+            ' His ten heads represent his knowledge of the six shastras and the four Vedas. To know about his Lanka ask Where is Lanka?';
         const repromptText = 'Ravana was a follower of Shiva, a great scholar, a capable ruler and a maestro of the veena, but someone who wished to overpower the Devas. ' +
-            'His ten heads represent his knowledge of the six shastras and the four Vedas.';
+            'His ten heads represent his knowledge of the six shastras and the four Vedas. To know about his Lanka ask Where is Lanka?';
         console.log(speechText);
-        //   handlerInput.attributesManager.setSessionAttributes({'name': 'investment'});
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(repromptText)
             .withSimpleCard(title, speechText)
             .getResponse();
+
     }
 };
 
@@ -214,8 +166,11 @@ const SessionEndedRequestHandler = {
     },
     handle(handlerInput) {
         const speech = 'Bye! Talk to you soon!';
+        const repromtTest = 'Bye! Talk to you soon!';
         return handlerInput.responseBuilder
             .speak(speech)
+            .repromt(repromtTest)
+            .withSimpleCard(title, speech)
             .getResponse();
     }
 };
@@ -232,23 +187,63 @@ const ErrorHandler = {
         console.log(`Error handled: ${error.message}`);
 
         return handlerInput.responseBuilder
-            .speak('Sorry, I can\'t understand the command. Please say again.')
-            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .speak('Sorry, I can`t understand the command. Please say again.')
+            .reprompt('Sorry, I can`t understand the command. Please say again.')
             .getResponse();
     }
 };
 
+/***
+ * Cancel Handler
+ * @type {{canHandle(): boolean, handle(*, *): *}}
+ */
+const CancelHandler = {
+    canHandle() {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'&&
+       handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent';
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+        console.log('cancel handlerrrr');
+        const speechText = 'Cancelling Event. Talk to you soon';
+        const repromptText = 'Oh! Talk to you soon';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(repromptText)
+            .withShouldEndSession(true)
+            .getResponse();
+    }
+};
+
+/***
+ * Help Handler
+ * @type {{canHandle(): boolean, handle(*, *): *}}
+ */
+const HelpHandler = {
+    canHandle() {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'&&
+       handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechtext = 'Oh! You can ask Lord Rama questions like Where is Lanka, who is Hanuman, who is Sita?';
+        return handlerInput.responseBuilder
+            .speak(speechtext)
+            .reprompt('Oh! Talk to you soon')
+            .getResponse();
+    }
+};
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
         AboutRamaHandler,
         BirthplaceHandler,
-        ParentalHistoryHandler,
         WifeDetailsHandler,
         HanumanHandler,
         LankaHandler,
-        FamilyHandler,
         RavanaHandler,
+        CancelHandler,
+        HelpHandler,
         SessionEndedRequestHandler,
     )
     .addResponseInterceptors()
